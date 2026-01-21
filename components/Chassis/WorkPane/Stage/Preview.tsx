cce2be28e8e70ad84135f36d5a58a56a1334c7bb
cobycloud <25079070+cobycloud@users.noreply.github.com>
2026-01-21 04:56:07 -0600

    Add markdown previews to git diff modes

--- components/Chassis/WorkPane/Stage/Preview.tsx
+++ components/Chassis/WorkPane/Stage/Preview.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { FileNode, AppTheme } from '../../../../../../types';
-import { getSyntaxThemeStyle } from '../../../../../../data/themes';
+import { FileNode, AppTheme } from '../../../../types';
+import { getSyntaxThemeStyle } from '../../../../data/themes';
 
 interface PreviewPaneProps {
   content: string;
